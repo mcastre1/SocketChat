f376a52936c058c259deb300e1aa48b35ee8ac85
mcastre1 <miguelcastrejongal@gmail.com>
2024-01-17 21:34:24 -0700

    Succesfully connected to local hosted db

---
+++ mysqltest.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+# Replace with your database details
+db_connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Mc255587!"
+)
+
+print(db_connection)
