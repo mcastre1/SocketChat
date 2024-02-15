d175e2058290963e831aed16b81898b583840f26
mcastre1 <miguelcastrejongal@gmail.com>
2024-02-15 06:58:23 -0700

    Created new class inside Message file

--- Message.py
+++ Message.py
@@ -3,3 +3,10 @@ class Message:
         self.msg = msg
         self.sender = sender
         self.recipient = recipient
+
+
+class NewConnection:
+    def __init__(self, msg, sender, recipient):
+        self.msg = msg
+        self.sender = sender
+        self.recipient = recipient
